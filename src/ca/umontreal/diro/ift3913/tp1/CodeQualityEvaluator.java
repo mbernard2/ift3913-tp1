f4a3f28c6f5786cbc12ea69dda1b099f22d0e308
Maxim Bernard <mb@mab.onl>
2022-02-07 13:15:32 -0500

    Build configuration

--- src/ca/umontreal/diro/ift3913/tp1/CodeQualityEvaluator.java
+++ src/ca/umontreal/diro/ift3913/tp1/CodeQualityEvaluator.java
@@ -3,6 +3,6 @@ package ca.umontreal.diro.ift3913.tp1;
 public class CodeQualityEvaluator {
 
     public static void main(String[] args) {
-	    // write your code here
+	    System.out.println("Hello, world!");
     }
 }
