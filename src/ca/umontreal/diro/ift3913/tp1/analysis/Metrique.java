4766d11d4eb7bcf9c797e2cd9da04799adf7b6ac
ss <46608210+sabrisam@users.noreply.github.com>
2022-02-08 20:30:33 -0500

    Proposition d'architecture
    Je n'arrive pas à bien comprendre ton architecture, je crois que la changer nous permettras d'accélérer un peu

---
+++ src/ca/umontreal/diro/ift3913/tp1/analysis/Metrique.java
@@ -0,0 +1,27 @@
+package ca.umontreal.diro.ift3913.tp1.analysis;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//voici comment je le voyais
+
+public class Metrique {
+	
+	public static int mesureLOCclass(File file, String nomMethode) throws FileNotFoundExeption{
+		int lignes =0;
+		Scanner scanner = new Scanner(file);
+		//...
+		return lignes;
+	}
+	
+	public static int mesureLOCclass(File file, String nomMethode) throws FileNotFoundExeption{
+		int lignes =0;
+		Scanner scanner = new Scanner(file);
+		//...
+		return lignes;
+	}
+	
+	public static float mesureDC(int methCLOC, int methLOC) {   //avec une autre classe methode ou il y aura les methCLOC, methLOC,etc
+		//return methCLOC/methLOC...
+	}
+	//...
+}
